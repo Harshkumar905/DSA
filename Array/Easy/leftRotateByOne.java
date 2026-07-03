77ca418a1a1e852fd7f35a0fece92c61d63b2c48
Harshkumar905 <harsh9058999@gmail.com>
2026-07-03 23:15:20 +0530

    removeDuplicate

--- Array/Easy/leftRotateByOne.java
+++ Array/Easy/leftRotateByOne.java
@@ -9,7 +9,6 @@ public class leftRotateByOne {
         arr[arr.length - 1] = temp;
         return arr;
     }
-
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 6 };
         arr = leftRotatebyOne(arr);
@@ -21,7 +20,7 @@ public class leftRotateByOne {
         }
     }
 }
-
+ 
 
 //TC = 0(n)
 //SC = 0(1)
---
+++ Array/Easy/removeDuplicates.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Arrays;
+
+public class removeDuplicates {
+  public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int j = 0;  // slow pointer
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1; // new length of unique array
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};
+        int newLength = removeDuplicates(arr);
+        System.out.println("Unique elements: " + Arrays.toString(Arrays.copyOf(arr, newLength)));
+    }
+}
+
+//TC = 0(n)
+//SC = 0(1)
